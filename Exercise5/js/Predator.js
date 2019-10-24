34de8f2a936f1e3befaf548a6751f234e5c68a96
xtale <xtalepod@riseup.net>
2019-10-23 20:56:17 -0400

    Ex.5 To count number of prey eaten by predator added this.preyEaten to constructor and updated the handleEating() method to include this.preyEaten ++ to count and console.log to make sure its happening.

--- Exercise5/js/Predator.js
+++ Exercise5/js/Predator.js
@@ -22,71 +22,66 @@ class Predator { // A Predator class describes what a Predator is and does
     // Check for player input and react appropriately
     if (keyIsDown(this.leftKey)) {
       this.vx = -this.speed;
-}
-else if (keyIsDown(this.rightKey)) {
-  this.vx = this.speed;
-}
-else {
-  this.vx = 0;
-}
-if (keyIsDown(this.upKey)) {
-  this.vy = -this.speed;
-}
-else if (keyIsDown(this.downKey)) {
-  this.vy = this.speed;
-}
-else {
-  this.vy = 0;
-}
+    } else if (keyIsDown(this.rightKey)) {
+      this.vx = this.speed;
+    } else {
+      this.vx = 0;
+    }
+    if (keyIsDown(this.upKey)) {
+      this.vy = -this.speed;
+    } else if (keyIsDown(this.downKey)) {
+      this.vy = this.speed;
+    } else {
+      this.vy = 0;
+    }
   }
 
-  move(){
-  this.x += this.vx;
-this.y += this.vy;
-this.health = this.health - this.healthLossPerMove;
-this.health = constrain(this.health,0,this.maxHealth);
-this.handleWrapping(); // Calls the handleWrapping method, note the use of "this"
-}
-
-
-handleWrapping() {
-if (this.x < 0) {
-  this.x += width;
-}
-else if (this.x > width) {
-  this.x -= width;
-}
-if (this.y < 0) {
-  this.y += height;
-}
-else if (this.y > height) {
-  this.y -= height;
-}
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.health = this.health - this.healthLossPerMove;
+    this.health = constrain(this.health, 0, this.maxHealth);
+    this.handleWrapping(); // Calls the handleWrapping method, note the use of "this"
   }
 
+
+  handleWrapping() {
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
 
   handleEating(prey) {
     // Check for an overlap with this prey
     // And reduce its health if there is one
     // Also increase the predator's health
-    let d = dist(this.x,this.y,prey.x,prey.y);
-  if (d < this.radius + prey.radius) {
-    this.health += this.healthGainPerEat;
-    this.health = constrain(this.health,0,this.maxHealth);
-    prey.health -= this.healthGainPerEat;
-  //this.preyEaten ++;
-    if (prey.health < 0) {
-      prey.reset();
+    let d = dist(this.x, this.y, prey.x, prey.y);
+    if (d < this.radius + prey.radius) {
+      this.health += this.healthGainPerEat;
+      this.health = constrain(this.health, 0, this.maxHealth);
+      prey.health -= this.healthGainPerEat;
+      //this.preyEaten ++;
+console.log('counting prey eaten');
+      this.preyEaten = this.preyEaten + 1;
+      if (prey.health < 0) {
+        prey.reset();
+      }
     }
-  }
   }
   display() {
     // Draw the predator on the canvas
     push();
-noStroke();
-fill(this.fillColor,this.health);
-this.radius = this.health;
-ellipse(this.x,this.y,this.radius * 2);
-pop();
+    noStroke();
+    fill(this.fillColor, this.health);
+    this.radius = this.health;
+    ellipse(this.x, this.y, this.radius * 2);
+    pop();
   }
 }
