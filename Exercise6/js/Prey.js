831f49718bdc794ab2d904f03992185b4b9f3a13
xtale <xtalepod@riseup.net>
2019-10-27 22:27:49 -0400

    Ex.6 handleWrapping typos fixed in Prey class.

--- Exercise6/js/Prey.js
+++ Exercise6/js/Prey.js
@@ -51,7 +51,8 @@ class Prey {
     this.tx += 0.01;
     this.ty += 0.01;
     // Handle wrapping
-    this.handleWarping();
+     //////////////// FIXED
+    this.handleWrapping();
  //////////////// FIXED
  }
     // handleWrapping
--- Exercise6/js/script.js
+++ Exercise6/js/script.js
@@ -48,6 +48,7 @@ function draw() {
   tiger.move();
   antelope.move();
   zebra.move();
+  bee.move();
 
   // Handle the tiger eating any of the prey
   tiger.handleEating(antelope);
