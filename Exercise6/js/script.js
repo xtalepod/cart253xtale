f27a5597e4ab771d3663a26abd07967e5c55a6ef
xtale <xtalepod@riseup.net>
2019-10-27 22:07:31 -0400

    Ex6. Script line 26, space required between function and setup.

--- Exercise6/js/script.js
+++ Exercise6/js/script.js
@@ -23,7 +23,7 @@ let bee;
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
-functionsetup() {
+function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
