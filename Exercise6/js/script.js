bb6d267e8122ea2cb9461619b4525cac49a510a0
xtale <xtalepod@riseup.net>
2019-10-27 22:09:03 -0400

    Ex.6 Script line 28, extra comma need to be removed

--- Exercise6/js/script.js
+++ Exercise6/js/script.js
@@ -25,7 +25,7 @@ let bee;
 // Creates objects for the predator and three prey
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
+  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
