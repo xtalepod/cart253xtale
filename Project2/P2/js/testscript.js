e84d2ab12be8b38f9b099718cf0b55c0c836b7ac
xtalepod <xtalepod@riseup.net>
2019-11-07 21:36:44 -0500

    P2: Cleaning up the test script to finally do away with objects I won't be using and removing old comments.

--- Project2/P2/js/testscript.js
+++ Project2/P2/js/testscript.js
@@ -88,36 +88,30 @@ let trees = [100, 25, 6, 20];
 //gameover or references state
 let gameOver = false;
 
-// let skyFall;
-// let skyFallImage;
-//
+//set anything that needs to be preloaded
 function preload() {
-  // skyFallImage = loadImage("assets/images/cone.png");
-
 }
 // setup()
 //
 // Sets up a canvas
-// Creates objects for the predator and three prey
+// Creates objects for the fox, the hedgehog, and the boxes
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // constructor(x, y, w, h,fillColor, speed, upKey, downKey, rightKey, leftKey) {
+  //creating the hedgohog(x, y, w, h,fillColor, speed, upKey, downKey, rightKey, leftKey) {
   hedgehog = new TestH(60, 300, 40, 40, color(26, 255, 140), 5, UP_ARROW, DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW);
-  // constructor(x, y,w,h, speed, radius, upKey, downKey, rightKey, leftKey) {
+  //creating the fox(x, y,w,h,fillColor, speed, upKey, downKey, rightKey, leftKey) {
   fox = new TestF(100, 40, 70, 70, color(153, 255, 204), 1, 87, 83, 65, 68, 70);
-  // (x, y, size, fillColor) {
+  //for loops to create the box objects (which are shaped like circles)
   push();
   for (let i = 0; i < 3; i++) {
     let x = 50 + 100 * i
     let y = 100 + 100 * i
     boxes.push(new TestB(x, y, 40, color(255, 153, 153)));
   }
-
   for (let i = 0; i < 3; i++) {
     let x = 850 - 100 * i
     let y = 100 + 100 * i
     boxes.push(new TestB(x, y, 40, color(255, 153, 153)));
-    //   // cone1 = new TestSkyFalling(100,100,5,30,skyFallImage);
     pop();
   }
 }
@@ -126,28 +120,27 @@ function setup() {
 //
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
-  // Clear the background to black
+  //set the background to a nice grey
   background(200);
-  // //
+  //if its start state displayStartScreen else if displayStoryScreen
+  //else if displayPlayScreen else if showGameOver
   if (state === "START") {
     displayStartScreen();
   } else if (state === "STORY") {
     displayStoryScreen();
-    // cone1.display();
-    // cone1.move();
   } else if (state === "PLAY") {
     displayPlayScreen();
-
+    //set variables for return true statement so the hedgehog and fox go back to their start stae after overlapping
     let hedgehogOverLapping = false;
     let foxOverLapping = false;
-
+    //for loop to display the total number of boxes and handleCollision w the hedgehog
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].display();
       if (boxes[i].handleCollision(hedgehog)) {
         hedgehogOverLapping = true;
       }
     }
-//
+    //for loop to display the total number of boxes and handleCollision w the fox
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].display();
       if (boxes[i].handleFoxCollision(fox)) {
@@ -155,12 +148,12 @@ function draw() {
       }
     }
 
-    //handle the input, movement, eating, and display for the hedgehog
+    //handle the input, movement, and display for the hedgehog
     hedgehog.handleInput();
     hedgehog.move();
     hedgehog.display(hedgehogOverLapping);
     // hedgehog.reset();
-    //handle the input, movement, eating, and display for the fox
+    //handle the input, movement, and display for the fox
     fox.handleInput();
     fox.move();
     fox.display(foxOverLapping);
@@ -179,68 +172,67 @@ function mousePressed() {
   } else if (state === "STORY") {
     if (mouseX > width / 2 && mouseX < width * 2 && mouseY > height / 2 && mouseY < height * 2) {
       state = "PLAY";
-
     }
   }
 }
 
 //story state
 function displayStoryScreen() {
-//set the background
+  //set the background
   background(204, 153, 255)
-//set the rectangle which will go behind phrase0
+  //set the rectangle which will go behind phrase0
   rectMode(CENTER, CENTER);
   fill(200);
   ellipse(width / 2, height / 2, 100);
-//set the font color for all text
-//declare a variable for story0 and its index
+  //set the font color for all text
+  //declare a variable for story0 and its index
   let phrase0 = story0[storyIndex0];
-//set the text variables for phrase0
+  //set the text variables for phrase0
   fill(20);
   textSize(30);
   textAlign(CENTER, CENTER);
   text(phrase0, width / 2, height / 2);
-//declare a variable for story1 and its index
+  //declare a variable for story1 and its index
   let phrase1 = story1[storyIndex1];
-//set the text variables for phrase0
+  //set the text variables for phrase0
 
   fill(0);
   textSize(10);
   text(phrase1, width / 2, 80);
-//set the text variables and text for button of the screen instructions
+  //set the text variables and text for button of the screen instructions
   push();
   fill(20);
   textFont('Courier New', [20]);
   textStyle(BOLD);
   text("press any key to start", width / 2, height / 1.1);
-  text("click the mouse to begin game play",width/2, height/1.06);
+  text("click the mouse to begin game play", width / 2, height / 1.06);
   pop();
 
   //a little hedgehog avatar for the story screen to better prepare the player
   //includes key instructions
-    noStroke();
-    fill(26, 255, 140);
-    rect(width/2 + 300, 110, 40, 40);
-    textFont('Courier New', [20]);
-    fill(5);
-    text("hedgehog", width / 2 + 300, 150);
-    text("UP, DOWN, LEFT, RIGHT", width / 2 + 300, 175);
+  noStroke();
+  fill(26, 255, 140);
+  rect(width / 2 + 300, 110, 40, 40);
+  textFont('Courier New', [20]);
+  fill(5);
+  text("hedgehog", width / 2 + 300, 150);
+  text("UP, DOWN, LEFT, RIGHT", width / 2 + 300, 175);
   //a little fox avatar for the story screen to better prepare the player
   //includes key instructions
-    noStroke();
-    fill(153, 255, 204);
-    rect(width/2 - 300, 100, 70, 70);
-    fill(5);
-    textFont('Courier New', [20]);
-    text("fox", width / 2 - 300, 150);
-    text("W,A,S,D + F", width / 2 - 300, 175);
+  noStroke();
+  fill(153, 255, 204);
+  rect(width / 2 - 300, 100, 70, 70);
+  fill(5);
+  textFont('Courier New', [20]);
+  text("fox", width / 2 - 300, 150);
+  text("W,A,S,D + F", width / 2 - 300, 175);
 
   //set the text variables instructions
-    fill(20);
-    textSize(10);
-    textStyle(BOLD);
-    text("instructions: get a pen and paper, explore the board", width / 2, height / 1.25);
-    text("write down what you read, do you understand now?",width / 2, height / 1.2)
+  fill(20);
+  textSize(10);
+  textStyle(BOLD);
+  text("instructions: get a pen and paper, explore the board", width / 2, height / 1.25);
+  text("write down what you read, do you understand now?", width / 2, height / 1.2)
 
 
   // corgi.move();
@@ -271,14 +263,14 @@ function displayStartScreen() {
   textStyle(BOLD);
   text("start", width / 1.1, height / 1.1);
   pop();
-//a little hedgehog avatar for the start screen to better prepare the player
+  //a little hedgehog avatar for the start screen to better prepare the player
   noStroke();
   fill(26, 255, 140);
-  rect(width/2 - 50, height/2 + 60, 40, 40);
-//a little fox avatar for the start screen to better prepare the player
+  rect(width / 2 - 50, height / 2 + 60, 40, 40);
+  //a little fox avatar for the start screen to better prepare the player
   noStroke();
   fill(153, 255, 204);
-  rect(width/2, height/2 + 30, 70, 70);
+  rect(width / 2, height / 2 + 30, 70, 70);
 
 }
 
@@ -290,7 +282,7 @@ function displayPlayScreen() {
     y = i * 40 + 40
     // rect(10, i * 40 + 40, trees[2], trees[3])
     for (let j = 0; j < 20; j++) {
-      fill(color(100,100,50));
+      fill(color(100, 100, 50));
       rect(j * .05 + 60 + j * 50, i * 40 + 40, trees[2], trees[3])
     }
   }
@@ -298,7 +290,6 @@ function displayPlayScreen() {
 }
 
 function keyPressed() {
-    console.log("why?");
   storyIndex0 += 1;
   storyIndex1 += 1;
   if (storyIndex0 >= story0.length) {
