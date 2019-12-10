cff87c7a0de853bc9a93be3a74de73f781662fe1
xtalepod <xtalepod@riseup.net>
2019-12-10 02:06:44 -0500

    P3: Start on the titleScene ;p

--- Project3/js/script.js
+++ Project3/js/script.js
@@ -25,7 +25,6 @@ let worldTwoBackground;
 //audio from milky video
 let milkyAudio;
 // //an array for textures pictures
-// let worldOneImages = [];
 
 function preload() {
 
@@ -50,7 +49,7 @@ function setup() {
 // setting up the key
   key = new Key(50, 50,40);
 
-  currentScene = worldTwo; // Because we start on the title
+  currentScene = titleScene; // Because we start on the title
 //a function to set up the portals
   setUpPortals();
 }
